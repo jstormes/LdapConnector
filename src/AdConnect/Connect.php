5d3386199b4c0aae6345b3b12501238c29ceef15
James Stormes <jstormes@stormes.net>
2021-06-07 18:17:30 -0500

    Attempt to catch errors.

--- src/AdConnect/Connect.php
+++ src/AdConnect/Connect.php
@@ -4,21 +4,22 @@ declare(strict_types=1);
 
 namespace JStormes\Ldap\AdConnect;
 
-use Psr\Log\LoggerInterface;
+use Exception;
 
 
 class Connect
 {
     private $ldapResource;
-    private $log;
     private $config=[];
 
-    public function __construct(string $server,
-                                LoggerInterface $log)
+    public function __construct(string $server)
     {
-        $this->log = $log;
-        $this->ldapResource = ldap_connect($server);
-        ldap_set_rebind_proc($this->ldapResource, array($this, 'rebind'));
+        if (($this->ldapResource = ldap_connect($server)) === false)
+            throw new Exception("LDAP-URI was not parseable");
+
+        if ((ldap_set_rebind_proc($this->ldapResource, array($this, 'rebind')))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+        
         $this->config = $this->searchBase('','(objectClass=*)',['*']);
     }
 
@@ -39,20 +40,34 @@ class Connect
     }
 
     public function searchBase($baseDN, $filter, $attributes) {
-        $searchResults = ldap_read($this->ldapResource, $baseDN, $filter, $attributes);
+        if (($searchResults = ldap_read($this->ldapResource, $baseDN, $filter, $attributes))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
         return $this->ldapParse($searchResults);
     }
 
     public function searchOneLevel($baseDN, $filter, $attributes) {
-        $searchResults = ldap_list($this->ldapResource, $baseDN, $filter, $attributes);
+        if (($searchResults = ldap_list($this->ldapResource, $baseDN, $filter, $attributes))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
         return $this->ldapParse($searchResults);
     }
 
     public function searchSubTree($baseDN, $filter, $attributes) {
-        $searchResults = ldap_search($this->ldapResource, $baseDN, $filter, $attributes);
+        if (($searchResults = ldap_search($this->ldapResource, $baseDN, $filter, $attributes))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
         return $this->ldapParse($searchResults);
     }
 
+    /**
+     * Puts the LDAP data into a more PHP friendly format so that "foreach(...)" can be used easer.
+     * Also changes Acitve Directory DateTime to PHP frienly DateTime.
+     * 
+     * @param $data
+     * @return array|mixed
+     * @throws \Exception
+     */
     private function parseLdapData($data)  {
         
         $returnVale = [];
@@ -68,10 +83,10 @@ class Connect
                 else {
                     //19870901000000Z date format parse
                     if(preg_match('/\\d{14}.0Z/', $data[$i])) {
-                        $returnVale[] = new \DateTime(strtotime($data[$i]));
+                        $returnVale[] = \DateTime::createFromFormat("YmdGis???", $data[$i]);
                     }
                     else if(preg_match('/\\d{14}Z/', $data[$i])) {
-                        $returnVale[] = new \DateTime(strtotime($data[$i]));
+                        $returnVale[] = \DateTime::createFromFormat("YmdGis?", $data[$i]);
                     }
                     else {
                         $returnVale[] = $data[$i];
@@ -92,7 +107,9 @@ class Connect
     }
     
     private function ldapParse($searchResults) {
-        $data = ldap_get_entries($this->ldapResource, $searchResults);
+        if (($data = ldap_get_entries($this->ldapResource, $searchResults))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+        
         $results = $this->parseLdapData($data);
         if (count($results)==1) {
             return $results[0];
