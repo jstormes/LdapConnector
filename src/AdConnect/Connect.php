452850f7c196d19955fe8770d5143e46780387fb
James Stormes <jstormes@stormes.net>
2021-06-08 14:57:44 -0500

    LDAP ReBind is working.

--- src/AdConnect/Connect.php
+++ src/AdConnect/Connect.php
@@ -11,16 +11,16 @@ class Connect
 {
     private $ldapResource;
     private $config=[];
+    
+    private $user;
+    private$password;
 
     public function __construct(string $server)
     {
         if (($this->ldapResource = ldap_connect($server)) === false)
             throw new Exception("LDAP-URI \"($server)\" was not parseable");
 
-        if ((ldap_set_rebind_proc($this->ldapResource, array($this, 'rebind')))===false)
-            throw new Exception(ldap_error($this->ldapResource));
-        
-        $this->config = $this->searchBase('','(objectClass=*)',['*']);
+        ldap_set_option($this->ldapResource, LDAP_OPT_PROTOCOL_VERSION, 3);
     }
 
     /**
@@ -29,6 +29,24 @@ class Connect
     public function getConfig()
     {
         return $this->config;
+    }
+
+    /**
+     * @param array $config
+     */
+    public function setConfig(array $config): void
+    {
+        $this->config = $config;
+    }
+
+    public function searchServerConfig()
+    {
+        return $this->searchBase('','(objectClass=*)',['*']);
+    }
+
+    public function searchNetBIOSNames()
+    {
+        return $this->searchSubTree('DC=digitalroominc,DC=com','(nETBIOSName=*)',['*']);
     }
 
     /**
@@ -57,12 +75,25 @@ class Connect
             $parts=explode(' ',$dataStr);
             $errorInHex = str_replace(',','',$parts[1]);
         }
-
+        
         return hexdec($errorInHex);
     }
 
+    /**
+     * https://ldapwiki.com/wiki/Ambiguous%20Name%20Resolution#section-Ambiguous+Name+Resolution-MicrosoftActiveDirectoryAndBindRequest
+     * 
+     * @param $user
+     * @param $password
+     * @throws Exception
+     */
     public function bind($user, $password) {
-        if ((@ldap_bind($this->ldapResource, $user, $password))===false){
+        
+        $this->user=$user;
+        $this->password=$password;
+
+        ldap_set_option($this->ldapResource, LDAP_OPT_PROTOCOL_VERSION, 3);
+
+        if ((ldap_bind($this->ldapResource, $user, $password))===false){
 
             if (ldap_get_option($this->ldapResource, LDAP_OPT_DIAGNOSTIC_MESSAGE, $extended_error)) {
                 echo "Error Binding to LDAP: $extended_error";
@@ -74,32 +105,55 @@ class Connect
             }
         }
 
-        throw new Exception(ldap_error($this->ldapResource));
+        ldap_set_option($this->ldapResource, LDAP_OPT_REFERRALS, 1);
+        if ((ldap_set_rebind_proc($this->ldapResource, [$this,'rebind']))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
+//        throw new Exception(ldap_error($this->ldapResource));
     }
 
     public function rebind($ldap, $referral) {
-        ldap_set_rebind_proc($ldap, 'rebind');
+
+        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldap, LDAP_OPT_REFERRALS, 1);
+        ldap_set_rebind_proc($ldap, [$this,'rebind']);
+
+        if (!ldap_bind($ldap, $this->user, $this->password)) {
+            echo "\n\nCould not bind to referral server: {$referral}\n\n";
+            return 1; // Yes, a 1 means a failure.
+        }
+        return 0; // Yes, return a 0 on success.
+    
     }
 
     public function searchBase($baseDN, $filter, $attributes) {
         if (($searchResults = ldap_read($this->ldapResource, $baseDN, $filter, $attributes))===false)
             throw new Exception(ldap_error($this->ldapResource));
 
-        return $this->ldapParse($searchResults);
+        if (($data = ldap_get_entries($this->ldapResource, $searchResults))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
+        return $this->ldapParse($data);
     }
 
     public function searchOneLevel($baseDN, $filter, $attributes) {
         if (($searchResults = ldap_list($this->ldapResource, $baseDN, $filter, $attributes))===false)
             throw new Exception(ldap_error($this->ldapResource));
 
-        return $this->ldapParse($searchResults);
+        if (($data = ldap_get_entries($this->ldapResource, $searchResults))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
+        return $this->ldapParse($data);
     }
 
     public function searchSubTree($baseDN, $filter, $attributes) {
         if (($searchResults = ldap_search($this->ldapResource, $baseDN, $filter, $attributes))===false)
             throw new Exception(ldap_error($this->ldapResource));
 
-        return $this->ldapParse($searchResults);
+        if (($data = ldap_get_entries($this->ldapResource, $searchResults))===false)
+            throw new Exception(ldap_error($this->ldapResource));
+
+        return $this->ldapParse($data);
     }
 
     /**
@@ -148,9 +202,7 @@ class Connect
         return $returnVale;
     }
     
-    private function ldapParse($searchResults) {
-        if (($data = ldap_get_entries($this->ldapResource, $searchResults))===false)
-            throw new Exception(ldap_error($this->ldapResource));
+    private function ldapParse($data) {
         
         $results = $this->parseLdapData($data);
         if (count($results)==1) {
